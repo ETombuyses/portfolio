ed1f90d4debfa86fdc04cbc61dafdf23a49b0dca
ETombuyses <e.tombuyses@gmail.com>
2019-03-04 23:22:33 +0100

    random decoration position

--- src/js/main.js
+++ src/js/main.js
@@ -1,14 +1,13 @@
+import { isAbsolute } from "path";
 
-
+//visible on croll
 function visibleOnScroll() {
   var scrollPosition = window.innerHeight + window.scrollY;
+  var emilie = document.querySelector('.about__profilePic');
+  var skills = document.querySelectorAll('.about__skills');
 
   window.requestAnimationFrame(function() {
-    
-    var emilie = document.querySelector('.about__profilePic');
-    var skills = document.querySelectorAll('.about__skills');
-
-
+   
     if (scrollPosition > emilie.offsetTop + 100) {
       emilie.classList.add('is-visible');
     }
@@ -23,4 +22,39 @@ function visibleOnScroll() {
 
 window.addEventListener('scroll', function() {
   visibleOnScroll();
-})
+});
+
+
+
+
+//random element on landing part
+
+function createRandomPolygons() {
+  var x = document.body.offsetWidth-80;
+  var y = document.querySelector('.landing').offsetHeight-20;
+  var randX = Math.floor(Math.random()*x);
+	var randY = Math.floor(Math.random()*y);
+
+  var polygon = document.createElement('img');
+  polygon.style.opacity = '0';
+  polygon.src = '/losange.8adcabfe.svg';
+  polygon.style.animation = 'visible 4s infinite alternate';
+  polygon.style.position = 'absolute';
+  polygon.style.height = '20px';
+  polygon.style.zindex = '0';
+  polygon.style.top = randY + 'px';
+  polygon.style.left = randX + 50 + 'px';
+  document.querySelector('.landing').appendChild(polygon);
+
+  setTimeout(function(){
+    polygon.remove();
+  }, 12000);
+}
+
+
+setInterval(function(){
+  createRandomPolygons();
+}, 500);
+  
+
+
